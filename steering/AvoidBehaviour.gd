f7623370c16278d694818292d2be6a690c6797ce
amiani <amianijohns@gmail.com>
2019-11-02 18:56:34 -0400

    removed state from avoidbehaviour

--- steering/AvoidBehaviour.gd
+++ steering/AvoidBehaviour.gd
@@ -1,9 +1,5 @@
 extends Behaviour
 class_name AvoidBehaviour
 
-var cars : Array
-func _init(cars:Array):
-  self.cars = cars
-
 func getDanger(position:TrackPosition, track:Track, cars, resolution:int):
   pass
