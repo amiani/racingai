86fd9a6e47d87a52bdd6d22450949b0e3504ad40
amiani <amianijohns@gmail.com>
2019-11-02 18:03:05 -0400

    added leftWidth and rightWidth

--- track/TrackNode.gd
+++ track/TrackNode.gd
@@ -1,10 +1,17 @@
 tool
 extends Node2D
+class_name TrackNode
 
 export var isStart = false
-export var width = 100
+export var leftWidth = 100
+export var rightWidth = 100
 export var index = 0
 var link : Vector2
+var linkLength setget setLinkLength, getLinkLength
+func setLinkLength(l):
+  print('stop it')
+func getLinkLength():
+  return link.length()
 var normal : Vector2
 var curvature = 0
 var color = '#fa37c6'
@@ -20,10 +27,10 @@ func _draw():
   draw_line(Vector2(), link, Color('#0066ff'))
   draw_line(Vector2(), normal*20, Color('#ff0000'))
 
-  var leftPoint = width*normal.rotated(-PI/2)
-  var rightPoint = width*normal.rotated(PI/2)
-  var nextLeftPoint = to_local(next.to_global(next.width * next.normal.rotated(-PI/2)))
-  var nextRightPoint = to_local(next.to_global(next.width * next.normal.rotated(PI/2)))
+  var leftPoint = leftWidth*normal.rotated(-PI/2)
+  var rightPoint = rightWidth*normal.rotated(PI/2)
+  var nextLeftPoint = to_local(next.to_global(next.leftWidth * next.normal.rotated(-PI/2)))
+  var nextRightPoint = to_local(next.to_global(next.leftWidth * next.normal.rotated(PI/2)))
   draw_line(leftPoint, rightPoint, Color('#000000'))
   draw_line(leftPoint, nextLeftPoint, Color('#000000'))
   draw_line(rightPoint, nextRightPoint, Color('#000000'))
