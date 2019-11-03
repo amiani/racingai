4c4fb7b983aae2192f0c6ff2e8f4333822f6da2d
amiani <amianijohns@gmail.com>
2019-11-03 10:09:39 -0500

    clamped slots

--- steering/AvoidBehaviour.gd
+++ steering/AvoidBehaviour.gd
@@ -2,6 +2,7 @@ extends Behaviour
 class_name AvoidBehaviour
 
 var carLength = 32
+var currDanger
 func getDanger(position:TrackPosition, track:Track, cars, resolution:int) -> Array:
   var danger = []
   for i in range(resolution):
@@ -10,6 +11,8 @@ func getDanger(position:TrackPosition, track:Track, cars, resolution:int) -> Arr
     var diff = c.global_position - position.toGlobal()
     var longDistance = (c.global_position - position.toGlobal()).dot(position.trackNode.link.normalized()) #TODO: project this on something other than the link
     if longDistance < carLength / 2:
-      danger[c.trackPosition.toSlot(resolution)] = 1
-      danger[c.trackPosition.toSlot(resolution)+1] = 1  #TODO: calculate car's slot span
+      danger[clamp(c.trackPosition.toSlot(resolution), 0, 9)] = 1
+      danger[clamp(c.trackPosition.toSlot(resolution)+1, 0, 9)] = 1  #TODO: calculate car's slot span
+      danger[clamp(c.trackPosition.toSlot(resolution)-1, 0, 9)] = 1  #TODO: calculate car's slot span
+  currDanger = danger
   return danger
