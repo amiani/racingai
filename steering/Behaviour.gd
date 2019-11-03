51bbcee401c289c8fda2a642317031d3f8dd0ecd
amiani <amianijohns@gmail.com>
2019-11-02 21:36:28 -0400

    removed extraneous range parameters

--- steering/Behaviour.gd
+++ steering/Behaviour.gd
@@ -3,12 +3,12 @@ class_name Behaviour
 
 func getInterest(position:TrackPosition, track:Track, cars, resolution:int) -> Array:
   var interest = []
-  for i in range(0, resolution):
+  for i in range(resolution):
     interest.append(0)
   return interest
 
 func getDanger(position:TrackPosition, track:Track, cars, resolution:int) -> Array:
   var danger = []
-  for i in range(0, resolution):
+  for i in range(resolution):
     danger.append(0)
   return danger
--- steering/RacingLineBehaviour.gd
+++ steering/RacingLineBehaviour.gd
@@ -4,9 +4,9 @@ class_name RacingLineBehaviour
 func getInterest(position:TrackPosition, track: Track, cars, resolution:int):
   var lineSlot = getLineSlot(position, resolution)
   var interest = []
-  for i in range(0, resolution):
+  for i in range(resolution):
     interest.append(-.044 * abs(lineSlot - i) + .5)
   return interest
   
 func getLineSlot(position:TrackPosition, resolution):
-  return resolution / 2 - 5
+  return resolution / 2 - 2
