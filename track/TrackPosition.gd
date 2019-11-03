200a701db47c147d4270acf33a8908b637cd77b2
amiani <amianijohns@gmail.com>
2019-11-02 21:37:32 -0400

    added toSlot method

--- track/TrackPosition.gd
+++ track/TrackPosition.gd
@@ -5,14 +5,21 @@ var long = 0
 var lat = 0
 var trackNode : TrackNode
 var trackWidth setget setTrackWidth, getTrackWidth
+
 func _init(trackNode:TrackNode, long=0, lat=0):
   self.trackNode = trackNode
   self.long = long
   self.lat = lat
 
-func toGlobal():
+func toGlobal() -> Vector2:
   var latNormal = (1-long)*trackNode.normal + long*trackNode.next.normal
   return trackNode.to_global(long*trackNode.link + lat*latNormal.rotated(PI/2))
+
+func toSlot(resolution):
+  var trackWidth = getTrackWidth()
+  var slotWidth = trackWidth / resolution
+  var latTranslated = lat + trackWidth / 2
+  return floor(latTranslated / slotWidth)
 
 func setTrackWidth(w):
   print("ERROR: Can't set track width")
